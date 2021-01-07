59197bb7845de36c9e202373cb3ea2093170a998
Kristaps <45151368+Kristaps92@users.noreply.github.com>
2021-01-07 20:51:02 +0200

    Add files via upload

--- TIC TAC TOE/script.js
+++ TIC TAC TOE/script.js
@@ -1,8 +1,22 @@
 function startGame(){
     document.turn = "X";
-    setMessage(document.turn + "gets to start");
+    setMessage(document.turn + " gets to start");
 }
 
 function setMessage(msg) {
     document.getElementById("messages").innerText = msg;
+}
+
+function nextMove(board) {
+    board.innerText = document.turn;
+    switchTurn();
+}
+
+function switchTurn() {
+    if (document.turn == "X"){
+        document.turn = "O";
+    } else {
+        document.turn = "X";
+    }
+    setMessage("It's " + document.turn + " 's turn!");
 }
